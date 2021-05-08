724d0b1d7e354a4812d48a87d92a94cba2fcb372
md-ansari-rushaan <78842845+md-ansari-rushaan@users.noreply.github.com>
2021-05-08 01:05:10 -0700

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+    // add code for displaying rest of the names and the hints.
+    fill("white")
+    textSize(15)
+    text("T O F N U N C I", 700,150)
+    fill("lightblue")
+    text("Hint: perform a particulur task", 700,170)
+    
+    fill("white")
+    textSize(15)
+    text("S A A A T D B E", 100,240)
+    fill("lightblue")
+    text("Hint: USE FOR Multi game code", 100,260)
+
+}
